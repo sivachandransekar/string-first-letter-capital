e56d9b894bd49a66408a8142be4906a4048c3acd
siva <sivasebi4545@gmail.com>
2023-07-28 16:50:50 +0530

    string-capital

---
+++ main.js
@@ -0,0 +1,5 @@
+var a=prompt("Enter the word");
+document.write("The given word is "+a+"<br>");
+var b=a.replace(a[0],a[0].toUpperCase());
+document.write("The capitalized word is ")
+document.write(b);
